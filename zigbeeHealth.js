25d90f1e076969a94d93c7f7a336244f5f6f4c19
Dijker <12412734+Dijker@users.noreply.github.com>
2021-03-06 22:36:37 +0100

    Update zigbeeHealth.js

--- zigbeeHealth.js
+++ zigbeeHealth.js
@@ -1,19 +1,23 @@
 /**
  * Check Homey's zigBee Health.
  * Please fill in two answers: 
- * rate your happiness / did you Reset Zigbee in v5 or start with v5?*/
-let userHappiness = 9            // Rate your Happiness with Zigbee in Homey Firmware v5 
+ * - rate your happiness
+ *   value 1-10 : 1 = poor 10 = Good/Best
+ * - did you Reset Zigbee in v5 or start with v5? 
+ *   value: true/false */
+let userHappiness = 9            // your Happiness with Zigbee in Homey Firmware v5 
 let userResetZigbeeOnv5 = false  // Did you Reset Zigbee in v5 or start with v5?
 /* 
  * Check your Homey's zigbee Health (and maybe fix?).
  *
  * @file    0.1-zigbeeHealth.js by Geurt Dijker
  * @author  Geurt Dijker <Homey.Apps@dijker.nu>
- * @version 1.0.0
+ * @version 1.0.1
  * @link    https://github.com/Dijker/ (Link to be ceated)
  * @since
  * @license GNU General Public License v3.0 @see distribution
- * 20210228 First published on Community forum 
+ * 20210307 First published on Community forum 
+ * 20210306 test badroute device logging 
  * 20210228 Addes Routing info 
  * 2021~~~~ Manny versions and hours before 1.0.0
  */
@@ -42,15 +46,16 @@ function main() {
   log ('# modelIds            : ' + objectLength( modelId ));
   log ('> modelIds            : ' + JSON.stringify( modelId )  );
   let routesPerHops = anlyzeRoutes(routes);
+  listBadRoutes(badRouteIDs);
   // log ('Routers    : ', routers);
   // listDevicenames();
   // log( 'Device Names       :', objectLength(devices) , ' ',JSON.stringify( getNames( devices )));
-  var enquete = [];
-  enquete.push( userHappiness, IEEEAddress, scriptVersion, userResetZigbeeOnv5, SysInfo.homeyModelName, SysInfo.homeyVersion, zigBeeState.controllerState.networkAddress, zigBeeState.controllerState.channel, zigBeeState.controllerState.chipVersion, zigBeeState.controllerState.transportRev, zigBeeState.controllerState.zstackVersion, nodes, objectLength( routers ), objectLength( endDevice ), objectLength( manufacturerName ),objectLength( modelId ))
-enquete = enquete.concat( '#', routesPerHops );
-  logLine('Zigbee enquete Data')
-  log ('> enquete            : ' , JSON.stringify(enquete) );
-
+  var resultObj = [];
+  resultObj.push( userHappiness, IEEEAddress, scriptVersion, userResetZigbeeOnv5, SysInfo.homeyModelName, SysInfo.homeyVersion, zigBeeState.controllerState.networkAddress, zigBeeState.controllerState.channel, zigBeeState.controllerState.chipVersion, zigBeeState.controllerState.transportRev, zigBeeState.controllerState.zstackVersion, nodes, objectLength( routers ), objectLength( endDevice ), objectLength( manufacturerName ),objectLength( modelId ))
+resultObj = resultObj.concat( '#', routesPerHops );
+  logLine('Zigbee Reporting Data')
+  resultStr = JSON.stringify(resultObj).replace('[','').replace(']','')
+  log ('> reporting            : ' , resultStr, ',$' );
 
   // log ('routes', routes)
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-[ Do NOT modify below this line!! ]-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -58,9 +63,10 @@ enquete = enquete.concat( '#', routesPerHops );
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-[ first get all Homey's info global ]-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 let scriptVersion = 1;
+let resultStr = '';
 let SysInfo = await Homey.system.getInfo();
 let HomeyName = await Homey.system.getSystemName();
-
+var badRouteIDs = [];
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-[ functions ]-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 function filterObjects(objs, _key, value) {
   var result=[];
@@ -117,12 +123,17 @@ function anlyzeRoutes(obj) {
     list.push(property)
   }
   // log ('NodeIDs = ', list );
+  var i = 0
   _(obj).forEach( function (route) { 
     // log ('(objectLength (route)', (objectLength (route)), route)
     result[(objectLength (route)+1)]++  ;
     _(route).forEach( function (routeID) { 
-      // log ('routeID XX ', ''+routeID, list.indexOf(''+routeID) )
-      if (list.indexOf(''+routeID) < 0) { result[0]++ }})
+      if (list.indexOf(''+routeID) < 0) { 
+        badRouteIDs.push(list[i])
+        result[0]++ 
+      }
+    })
+    i++
   })
   log ('# hops used for device-' , [ 'B',0,1,2,3,4] );
   log ('result # Routes/hops  :' , result );
@@ -161,8 +172,31 @@ Number.prototype.toDDHHMMSS = function() {
     ].filter(s => s).join(':');
 }
 
+function getDeviceName(deviceID) {
+  var result = '';
+  _(zigBeeState.nodes).forEach( function (node ) { 
+    if ( node.nwkAddr == deviceID ) {
+      // log ( 'BadRoute Node Name = ' + node.name )
+      result = node.name
+    }
+  })
+  return result
+};
+
+function listBadRoutes (badIDs) {
+  logLine ('Devices with Error in Route')
+  // log ('(badIDs:', badIDs)
+  _(badIDs).forEach( function (ID) {
+    var devName = getDeviceName (ID);
+    if (devName === '') {
+      devName = 'Unknown device'
+    }
+    log ( 'Error in route from: ' + devName );
+  })
+};
+
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-[ call main ]-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 let  zigBeeState = await Homey.zigBee.getState();
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-[ call main ]-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 main();
-return 'Thanks'
+return 'Please report above string back on Community'
