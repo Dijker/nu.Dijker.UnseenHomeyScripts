6b9161c6c0cd0868a4065a2f39bd443866471760
Dijker <12412734+Dijker@users.noreply.github.com>
2021-03-07 13:50:38 +0100

    Update zigbeeHealth.js

--- zigbeeHealth.js
+++ zigbeeHealth.js
@@ -7,17 +7,19 @@
  *   value: true/false */
 let userHappiness = 9            // your Happiness with Zigbee in Homey Firmware v5 
 let userResetZigbeeOnv5 = false  // Did you Reset Zigbee in v5 or start with v5?
-let anonymizeNames = false       // Show IDs, not names
+let anonymizeNames = false       // Show IDs, not names - value: true/false 
 /* 
  * Check your Homey's zigbee Health (and maybe fix?).
  *
  * @file    0.1-zigbeeHealth.js by Geurt Dijker
  * @author  Geurt Dijker <Homey.Apps@dijker.nu>
- * @version 1.0.5
+ * @version 1.0.6
  * @link    https://github.com/Dijker/ (Link to be ceated)
  * @since
  * @license GNU General Public License v3.0 @see distribution
  * 20210307 First published on Community forum 
+ * 20210307 Added Tips/Link for No Avtive Routers  
+ * 20210306 Added logging active Routers (Thanks @Ted for sugestion)  
  * 20210306 test badroute device logging 
  * 20210228 Addes Routing info 
  * 2021~~~~ Manny versions and hours before 1.0.0
@@ -49,13 +51,20 @@ function main() {
   logLine ('3b: routing is an ephemeral state and may change at any time ')
   log('# hops used for device-', ['B', 0, 1, 2, 3, 4]);
   log('result # Routes/hops  :', routesPerHops);
-  logLine('4: Devices with Error in Route')
-  listDevices(badRouteIDs);
+  if (objectLength(badRouteIDs) > 0) {
+    logLine('4: Devices with Error in Route')
+    listDevices(badRouteIDs);
+  }
   logLine('5: Devices active Routing')
-  listDevices(activeRouteIDs);
-
+  if (objectLength(activeRouteIDs) > 0) {
+    listDevices(activeRouteIDs);
+  } else { 
+    log ('Please add zigBee Routers to create a mesh network')
+    log ('For more info read \n' + 
+    ' https://support.athom.com/hc/en-us/articles/360019239879-Advice-on-building-a-stable-Zigbee-network')
+  }
   var resultObj = [];
-  resultObj.push(userHappiness, IEEEAddress, scriptVersion, userResetZigbeeOnv5, SysInfo.homeyModelName,
+  resultObj.push( userHappiness, IEEEAddress, scriptVersion, userResetZigbeeOnv5, SysInfo.homeyModelName,
                   SysInfo.homeyVersion, zigBeeState.controllerState.networkAddress, 
                   zigBeeState.controllerState.channel, zigBeeState.controllerState.chipVersion,
                   zigBeeState.controllerState.transportRev, zigBeeState.controllerState.zstackVersion,
@@ -64,7 +73,7 @@ function main() {
   resultObj = resultObj.concat('#', routesPerHops);
   logLine('6: Zigbee Reporting Data')
   resultStr = JSON.stringify(resultObj).replace('[', '').replace(']', '')
-  log('> reporting            : ', resultStr, ',$');
+  log('> reporting : ', resultStr, ', $');
 
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-[ Do NOT modify below this line!! ]-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 }; // end of main()
